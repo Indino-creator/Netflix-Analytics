5ffb8505295c60628b75f7f4925295aec06a90b9
Armaan <armaan2422e@gmail.com>
2026-08-20 12:55:32 +0530

    Fix data loading path and add cleaned dataset file
    - Updated the data loading function to reference the correct path for the Netflix titles dataset.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,8 +1,6 @@
 import pandas as pd 
-
+# Here we will load data fo further cleanin process 
 
 def data_loader():
-    Data = pd.read_csv("Datasets/netflix_titles.csv")
+    Data = pd.read_csv("../Datasets/netflix_titles.csv")
     return Data
-
-data_loader()
